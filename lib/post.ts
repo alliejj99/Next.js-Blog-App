f2efdd39995ff537e42f18a76542ffcca37a4d89
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-03-28 22:04:14 +0900

    마크다운 파일을 데이터로 추출하는 법

---
+++ lib/post.ts
@@ -0,0 +1,35 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export function getSortedPostsData() {
+  // ./poosts 파일 이름 추적
+
+  const fileNames = fs.readdirSync(postsDirectory);
+  // ['pre-rendering.md', ...]
+
+  const allPostsData = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.md$/, ""); // 불러올때 확장자 .md 생략
+
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf-8"); // 인코딩
+
+    const metterResult = matter(fileContents);
+
+    return {
+      id,
+      ...allPostsData(metterResult.data as { date: string; title: string }),
+    };
+  });
+
+  // Sort 정렬
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
